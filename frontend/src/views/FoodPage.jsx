4fad29e89de5ed174e483801e8cfc93e84451d9d
twija <kassdarmofal@gmail.com>
2020-01-28 22:01:48 +0100

    added list of food items

--- frontend/src/views/FoodPage.jsx
+++ frontend/src/views/FoodPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import FoodItem from './FoodItem.jsx'
+import * as data from '../data.json'
 
 class FoodPage extends React.Component {
     render() {
+        const dataFoodList = data.food.map( (e, index) => <FoodItem name={e.name} photo={e.photo} key={index}/>)
         return (
             <div>
-                <FoodItem/>
+                {dataFoodList}
             </div>
         )
     }
