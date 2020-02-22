42afae2b7de13afbf7d781aeb349b4fc355ae9a3
twija <kassdarmofal@gmail.com>
2020-02-22 13:18:10 +0100

    added Grid system and foodItem actions

--- frontend/src/views/FoodItem.jsx
+++ frontend/src/views/FoodItem.jsx
@@ -1,18 +1,70 @@
 import React from 'react'
 import Card from '@material-ui/core/Card'
 import makeStyles from "@material-ui/core/styles/makeStyles";
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid'
 
 const useStyles = makeStyles({
     card: {
-        maxWidth: 345,
+        maxWidth: 345
+    },
+    actionArea: {
+        height: 150
     }
 });
 
-function FoodItem({name, photo}) {
+function FoodItem({name, photo, price}) {
     const classes = useStyles();
     return (
         <Card className={classes.card}>
-            {name}
+            <CardActionArea>
+                <CardMedia
+                    square
+                    image={photo}
+                    title={name}
+                    className={classes.actionArea}
+                />
+                <CardContent>
+                    <Typography
+                        gutterBottom
+                        variant='h5'
+                        component='h2'>
+                        {name}
+                    </Typography>
+                    <Typography
+                        variant='body2'
+                        color='textSecondary'
+                        component='p'>
+                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                        across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button
+                    size='small'
+                    color='primary'>
+                    Share
+                </Button>
+                <Button
+                    size='small'
+                    color='primary'>
+                    Share
+                </Button>
+                <Grid container justify='space-around'>
+                    <Typography align='center'>
+                        Price
+                    </Typography>
+                    <Typography align='right'>
+                        {Number.parseFloat(price).toFixed(2)} zł
+                    </Typography>
+                </Grid>
+            </CardActions>
         </Card>
     )
 }
--- frontend/src/views/FoodPage.jsx
+++ frontend/src/views/FoodPage.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import FoodItem from './FoodItem.jsx'
 import * as data from '../data.json'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
 
 class FoodPage extends React.Component {
     render() {
-        const dataFoodList = data.food.map( (e, index) => <FoodItem name={e.name} photo={e.photo} key={index}/>)
+        const dataFoodList = data.food.map((e, index) => {
+            return (
+                <Grid item xs={12} sm={4}>
+                    <FoodItem name={e.name} photo={e.photo} price={e.price} key={index}/>
+                </Grid>)
+        });
         return (
-            <div>
-                {dataFoodList}
-            </div>
+            <Grid container spacing={3}>
+                    {dataFoodList}
+            </Grid>
         )
     }
 }
