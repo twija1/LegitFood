b3e05f8504fa1cd3b73b3acf66182d6cc339cbd7
twija1 <twija1@users.noreply.github.com>
2020-09-20 16:51:32 +0000

    deploy: d6933500cafb502dd9565ea1e4be940a63ff1feb

---
+++ precache-manifest.7d24e0bb92c275b1129b79b4381ec576.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c24c5c2727134d9ce0fed71a08214843",
+    "url": "/LegitFood/index.html"
+  },
+  {
+    "revision": "b423bdd6c9dc32199b35",
+    "url": "/LegitFood/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8b28ef86a29d9416b310",
+    "url": "/LegitFood/static/js/2.71516f5e.chunk.js"
+  },
+  {
+    "revision": "90ed0728a866ab94f833cfdc372ae5e8",
+    "url": "/LegitFood/static/js/2.71516f5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b423bdd6c9dc32199b35",
+    "url": "/LegitFood/static/js/main.3c3831a0.chunk.js"
+  },
+  {
+    "revision": "26f1c4d8f3c6c3e69f4c",
+    "url": "/LegitFood/static/js/runtime-main.1f9cd6a3.js"
+  }
+]);
